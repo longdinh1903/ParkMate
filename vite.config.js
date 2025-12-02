c929d234bdd7b9d9076ddf0dba4b49443dbc3514
KS_REACT_SE1722116 <longdt46@fpt.com>
2025-12-02 10:32:41 +0700

    vercel fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,17 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
   server: {
     open: true,
     proxy: {
